1af218a77ade85431e381acb40af97a48432b3db
Ayush Maiti <ayush.maiti39@gmail.com>
2025-12-25 05:40:18 +0530

    setup d13

---
+++ debugging.py
@@ -0,0 +1,33 @@
+# BROKEN CODE BLOCK 1
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+    b_list.append(new_item)
+    print(b_list)
+
+mutate([1, 2, 3, 5, 8, 13])
+
+# BROKEN CODE BLOCK 2
+year = int(input("Which year do you want to check? "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
+  
+# BROKEN CODE BLOCK 3
+for number in range(1, 101):
+    if number % 3 == 0 or number % 5 == 0:
+        print("FizzBuzz")
+    if number % 3 == 0:
+        print("Fizz")
+    if number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
